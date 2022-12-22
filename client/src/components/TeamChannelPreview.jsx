78949ebf2f02847dd295548850f9b8d99cf4f619
kingking1223 <kingking1223@gmail.com>
2022-12-22 16:32:45 +0800

    Create login page

--- client/src/components/TeamChannelPreview.jsx
+++ client/src/components/TeamChannelPreview.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Avatar, useChatContext } from 'stream-chat-react;'
+import { Avatar, useChatContext } from 'stream-chat-react';
 
 const TeamChannelPreview = ({ channel, type }) => {
     const { channel: activeChannel, client } = useChatContext();
@@ -12,11 +12,30 @@ const TeamChannelPreview = ({ channel, type }) => {
 
     const DirectPreview = () => {
         const members = Object.values(channel.state.members).filter(({ user }) => user.id !== client.userID);
+
+        return (
+            <div className='channel-preview__item single'>
+                <Avatar 
+                    image={members[0]?.user.image}
+                    name={members[0]?.user.fullname}
+                    size={24}
+                />
+                <p>{members[0]?.user?.fullname}</p>
+            </div>
+        )
     }
 
     return (
-        <div>
-
+        <div className={
+            channel?.id === activeChannel?.id
+                ? 'channel-preview__wrapper__selected'
+                : 'channel-preview__wrapper'
+        }
+        onclick={() => {
+            console.log(channel)
+        }}
+        >
+            {type === 'team' ? <ChannelPreview /> : <DirectPreview />}
         </div>
     )
 }
---
+++ server/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+
+const authRoutes = require("./routes/auth.js")
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+require('dotenv').config();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded());
+
+app.get('/', (req, res) => {
+    res.send('hello world');
+})
+
+app.listen(port, () => console.log(`Server now running on port ${port}`));
