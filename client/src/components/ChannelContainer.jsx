dd6d726b7494304cf553b30b61b838c89376b1b3
kingking1223 <kingking1223@gmail.com>
2022-12-23 18:10:05 +0800

    Create channel display

--- client/src/components/ChannelContainer.jsx
+++ client/src/components/ChannelContainer.jsx
@@ -1,9 +1,45 @@
 import React from 'react'
+import { Channel, useChatContext, MessageTeam } from 'stream-chat-react'
 
-const ChannelContainer = () => {
-  return (
-    <div>ChannelContainer</div>
-  )
+import { ChannelInner, CreateChannel, EditChannel, TeamMessage } from './'
+
+const ChannelContainer = ({ isCreating, setIsCreating, isEditing, setIsEditing, createType }) => {
+    const { channel } = useChatContext();
+
+    if (isCreating) {
+        return (
+            <div className="channel_container">
+                <CreateChannel createType={createType} setIsCreating={setIsCreating} />
+            </div>
+        )
+    }
+
+    if (isEditing) {
+        return (
+            <div className="channel_container">
+                <EditChannel setIsCreating={setIsCreating} />
+            </div>
+        )
+    }
+
+    const EmptyState = () => (
+        <div className="channel-empty__container">
+            <p className="channel-empty__first">This is the the start of your chat.</p>
+            <p className="channel-empty__second">Send message from here.</p>
+
+        </div>
+    )
+
+    return (
+        <div className="channel__container">
+            <Channel
+                EmptyStateIndicator={EmptyState}
+                Message={(messageProps, i) => <MessageTeam key={i} {...messageProps} />}
+            >
+                <ChannelInner setIsEditing={setIsEditing}/>
+            </Channel>
+        </div>
+    )
 }
 
 export default ChannelContainer
