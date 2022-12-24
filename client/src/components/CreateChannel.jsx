8e369eed0eb137184584b25edc8ae5c800f0d362
kingking1223 <kingking1223@gmail.com>
2022-12-24 12:20:18 +0800

    Create add channel button and view

--- client/src/components/CreateChannel.jsx
+++ client/src/components/CreateChannel.jsx
@@ -1,11 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useChatContext } from 'stream-chat-react'
 
-const CreateChannel = () => {
-  return (
-    <div>
-        
-    </div>
-  )
+import { UserList } from './'
+import { CloseCreateChannel } from '../assets'
+
+const ChannelNameInput = ({ channelName = '', setChannelName, }) => {
+    const handleChange = (e) => {
+        e.preventDefault()
+        setChannelName(e.target.value)
+    }
+
+    return (
+        <div className='channel-name-input__wrapper'>
+            <p>Name</p>
+            <input value={channelName} onChange={handleChange} placeholder="Channel Name" />
+            <p>Add Members</p>
+        </div>
+    )
+}
+
+const CreateChannel = ({ createType, setIsEditing}) => {
+    const [ channelName, setChannelName ] = useState('')
+
+    return (
+        <div className='create-channel__containe'>
+            <div className="create-channel__header">
+                <p>{createType === "team" ? 'Create a New Channel' : "Send a Direct Message"}</p>
+                <CloseCreateChannel setIsCreating={setIsEditing} />
+            </div>
+            {createType === "team" && <ChannelNameInput channelName={channelName} setChannelName={setChannelName}/>}
+            <UserList />
+        </div>
+    )
 }
 
 export default CreateChannel
